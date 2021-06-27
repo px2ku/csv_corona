651c62add00f2943e63d9d2bf86cef804f91fe3f
px2ku <54834452+px2ku@users.noreply.github.com>
2021-06-28 00:58:27 +0200

    StopinJerkin

--- src/main/java/boran/CsvReader.java
+++ src/main/java/boran/CsvReader.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CsvReader {
@@ -16,20 +17,38 @@ public class CsvReader {
 
     public List<DataPoint> readDataPoints() {
         //2-480
+        List<DataPoint> dataPoints = new ArrayList<>();
         try {
             List<String> lines = Files.readAllLines(Path.of(coronaCsv.getPath()));
-            for (int rowIndex = 0; rowIndex < 480; rowIndex += 1) {
-                String currentLine = lines.get(rowIndex);
-                System.out.println(currentLine);
 
-            } //Welche Zeile zu starten
+            for (String line : lines) {
 
+                DataPoint filteredLine = DataPoint.readLine(line);
+
+                if (filteredLine != null) {
+                    dataPoints.add(filteredLine);
+                    System.out.println(filteredLine);
+                }
+
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
         }
 
-
+        return dataPoints;
     }
 }
+
+//            for (int rowIndex = 1; rowIndex < 480; rowIndex += 1) {
+//                String currentLine = lines.get(rowIndex);
+//
+//                DataPoint.readLine(currentLine);
+//
+//
+//                DataPoint filteredLine = DataPoint.readLine(currentLine);
+//                System.out.println(filteredLine);
+//
+//
+//            } //Welche Zeile zu starten
